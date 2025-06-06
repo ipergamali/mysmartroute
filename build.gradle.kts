326c4094b2ad6405adf4316d57c55384a1f1ef24
ipergamali <joanneper@hotmail.com>
2025-06-07 01:59:07 +0300

    Add Compose compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-    plugins {
-        id("com.android.application") version "8.9.2" apply false
-        id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-        //id("org.jetbrains.kotlin.plugin.compose") version "1.5.11" apply false
-    // Compose plugin is not required for this project
-
-        id("com.google.gms.google-services") version "4.4.2" apply false
-    }
+plugins {
+    id("com.android.application") version "8.9.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.5.11" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
