f473eb733056a196bb14e2095f42f7a1a5c10f08
ipergamali <joanneper@hotmail.com>
2025-06-06 16:08:36 +0300

    Add Kotlin implementations for core transport models

---
+++ app/src/main/java/com/ioannapergamali/mysmartroute/model/classes/poi/BasicPoI.kt
@@ -0,0 +1,16 @@
+package com.ioannapergamali.mysmartroute.model.classes.poi
+
+import com.ioannapergamali.mysmartroute.model.enumerations.PoIType
+import com.ioannapergamali.mysmartroute.model.interfaces.PoI
+
+/**
+ * Basic implementation of [PoI] used to represent a simple point of interest.
+ */
+data class BasicPoI(
+    override val id: String,
+    override val description: String,
+    override val type: PoIType
+) : PoI {
+    override fun getType(): PoIType = type
+}
+
