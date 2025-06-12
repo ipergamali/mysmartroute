95677dc88febaf97f990d43ca84c2a1449a3f252
ipergamali <joanneper@hotmail.com>
2025-06-12 07:04:49 +0300

    Add custom OpenSans font

--- app/src/main/java/com/ioannapergamali/mysmartroute/view/ui/AppFont.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/view/ui/AppFont.kt
@@ -1,10 +1,20 @@
 package com.ioannapergamali.mysmartroute.view.ui
 
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.ioannapergamali.mysmartroute.R
 
 enum class AppFont(val label: String, val fontFamily: FontFamily) {
     SansSerif("Sans Serif", FontFamily.SansSerif),
     Serif("Serif", FontFamily.Serif),
     Monospace("Monospace", FontFamily.Monospace),
-    Cursive("Cursive", FontFamily.Cursive)
+    Cursive("Cursive", FontFamily.Cursive),
+    OpenSans(
+        "Open Sans",
+        FontFamily(
+            Font(R.font.opensans_regular, FontWeight.Normal),
+            Font(R.font.opensans_bold, FontWeight.Bold)
+        )
+    )
 }
