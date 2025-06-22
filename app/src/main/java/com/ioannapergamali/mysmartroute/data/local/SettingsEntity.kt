7695d09277469fcec6a236015ab45bf37c0dbe0d
ipergamali <joanneper@hotmail.com>
2025-06-22 13:16:39 +0300

    Προσθήκη αναφορών ForeignKey και references

--- app/src/main/java/com/ioannapergamali/mysmartroute/data/local/SettingsEntity.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/data/local/SettingsEntity.kt
@@ -1,11 +1,20 @@
 package com.ioannapergamali.mysmartroute.data.local
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "settings",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
     indices = [Index("userId")]
 )
 data class SettingsEntity(
