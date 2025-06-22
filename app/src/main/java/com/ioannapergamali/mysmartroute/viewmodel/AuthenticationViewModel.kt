4cc0b43e7c98b71b0756ef03892db3e4d23e28ca
ipergamali <joanneper@hotmail.com.com>
2025-06-22 19:23:07 +0300

    log

--- app/src/main/java/com/ioannapergamali/mysmartroute/viewmodel/AuthenticationViewModel.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/viewmodel/AuthenticationViewModel.kt
@@ -2,6 +2,7 @@ package com.ioannapergamali.mysmartroute.viewmodel
 
 import android.app.Activity
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
