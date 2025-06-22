8b807a9e67617e3c04828dc95611210114d3304d
ipergamali <joanneper@hotmail.com>
2025-06-22 22:24:42 +0300

    feat: init roles and menus on app start

--- app/src/main/java/com/ioannapergamali/mysmartroute/MySmartRouteApplication.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/MySmartRouteApplication.kt
@@ -5,12 +5,14 @@ import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.ioannapergamali.mysmartroute.BuildConfig
 import com.ioannapergamali.mysmartroute.utils.ShortcutUtils
+import com.ioannapergamali.mysmartroute.viewmodel.AuthenticationViewModel
 
 
 class MySmartRouteApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
+        AuthenticationViewModel().ensureMenusInitialized(this)
         // Η υπηρεσία Firebase App Check απενεργοποιήθηκε προσωρινά
         //val apiKey = BuildConfig.MAPS_API_KEY
 //        Log.d("MySmartRoute Maps API key ", "Maps API key loaded: ${apiKey.isNotBlank()}")
