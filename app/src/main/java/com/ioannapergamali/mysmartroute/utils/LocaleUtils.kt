8b9dc151beda57d5a0a507a902cb02428ddf0ab3
ipergamali <joanneper@hotmail.com>
2025-06-25 12:13:30 +0300

    Update locale handling using AppCompatDelegate

--- app/src/main/java/com/ioannapergamali/mysmartroute/utils/LocaleUtils.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/utils/LocaleUtils.kt
@@ -1,15 +1,12 @@
 package com.ioannapergamali.mysmartroute.utils
 
 import android.content.Context
-import android.content.res.Configuration
-import java.util.Locale
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 
 object LocaleUtils {
     fun updateLocale(context: Context, language: String) {
-        val locale = Locale(language)
-        Locale.setDefault(locale)
-        val config = Configuration(context.resources.configuration)
-        config.setLocale(locale)
-        context.resources.updateConfiguration(config, context.resources.displayMetrics)
+        val locales = LocaleListCompat.forLanguageTags(language)
+        AppCompatDelegate.setApplicationLocales(locales)
     }
 }
