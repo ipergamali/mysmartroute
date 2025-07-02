2af23852cca3c68265c7c383e97898cece3316e7
ipergamali <joanneper@hotmail.com>
2025-07-02 20:29:37 +0300

    Fix POI dropdown to focus text input

--- app/src/main/java/com/ioannapergamali/mysmartroute/view/ui/screens/AnnounceTransportScreen.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/view/ui/screens/AnnounceTransportScreen.kt
@@ -390,14 +390,9 @@ fun AnnounceTransportScreen(navController: NavController, openDrawer: () -> Unit
             ExposedDropdownMenuBox(
                 expanded = fromExpanded,
                 onExpandedChange = {
-                    if (fromQuery.isNotBlank()) {
-                        val expand = !fromExpanded
-                        fromExpanded = expand
-                        if (expand) {
-                            fromFocusRequester.requestFocus()
-                            keyboardController?.show()
-                        }
-                    }
+                    fromExpanded = false
+                    fromFocusRequester.requestFocus()
+                    keyboardController?.show()
                 },
                 modifier = Modifier.weight(1f)
             ) {
@@ -582,14 +577,9 @@ fun AnnounceTransportScreen(navController: NavController, openDrawer: () -> Unit
             ExposedDropdownMenuBox(
                 expanded = toExpanded,
                 onExpandedChange = {
-                    if (toQuery.isNotBlank()) {
-                        val expand = !toExpanded
-                        toExpanded = expand
-                        if (expand) {
-                            toFocusRequester.requestFocus()
-                            keyboardController?.show()
-                        }
-                    }
+                    toExpanded = false
+                    toFocusRequester.requestFocus()
+                    keyboardController?.show()
                 },
                 modifier = Modifier.weight(1f)
             ) {
