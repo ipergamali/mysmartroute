c63a149992d873ce8dc826a2829a03ea24a1630a
ipergamali <joanneper@hotmail.com>
2025-07-08 04:44:17 +0300

    Add fallback menuAnchor extension

---
+++ app/src/main/java/androidx/compose/material3/MenuAnchorFallback.kt
@@ -0,0 +1,9 @@
+package androidx.compose.material3
+
+import androidx.compose.ui.Modifier
+
+/**
+ * Fallback υλοποίηση της [menuAnchor] για εκδόσεις του Compose που δεν
+ * περιλαμβάνουν τη συνάρτηση.
+ */
+fun Modifier.menuAnchor(): Modifier = this
