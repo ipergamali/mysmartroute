8d2d2b5e3bd002835dd277d4058f82e63338c177
ipergamali <joanneper@hotmail.com>
2025-07-16 23:53:46 +0300

    Προσθήκη bubble πληκτρολογίου και αυτόματης κύλισης

---
+++ app/src/main/java/com/ioannapergamali/mysmartroute/view/ui/util/KeyboardBubbleState.kt
@@ -0,0 +1,18 @@
+package com.ioannapergamali.mysmartroute.view.ui.util
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
+
+/**
+ * Κράτημα κατάστασης του κειμένου που εμφανίζεται στο bubble.
+ */
+class KeyboardBubbleState {
+    var text by mutableStateOf("")
+    var activeFieldId by mutableStateOf<Int?>(null)
+}
+
+@Composable
+fun rememberKeyboardBubbleState() = remember { KeyboardBubbleState() }
