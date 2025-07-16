9ee7276fa13e3a0630dea008ba5c13487b610ca9
ipergamali <joanneper@hotmail.com>
2025-07-16 17:46:50 +0300

    Show color picker dialog

--- app/src/main/java/com/ioannapergamali/mysmartroute/model/enumerations/VehicleColor.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/model/enumerations/VehicleColor.kt
@@ -1,11 +1,13 @@
 package com.ioannapergamali.mysmartroute.model.enumerations
 
-/** Διαθέσιμα χρώματα οχημάτων. */
-enum class VehicleColor(val label: String) {
-    BLACK("Black"),
-    WHITE("White"),
-    RED("Red"),
-    BLUE("Blue"),
-    GREEN("Green"),
-    YELLOW("Yellow")
+import androidx.compose.ui.graphics.Color
+
+/** Διαθέσιμα χρώματα οχημάτων με αντίστοιχο χρώμα για προεπισκόπηση. */
+enum class VehicleColor(val label: String, val color: Color) {
+    BLACK("Black", Color.Black),
+    WHITE("White", Color.White),
+    RED("Red", Color.Red),
+    BLUE("Blue", Color.Blue),
+    GREEN("Green", Color(0xFF4CAF50)),
+    YELLOW("Yellow", Color(0xFFFFEB3B))
 }
