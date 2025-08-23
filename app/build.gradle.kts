39ac4399fafb4642ac031e09e7393bada0ef3b49
ipergamali <joanneper@hotmail.com>
2025-08-23 16:39:46 +0300

    chore: ενημέρωση βασικών εξαρτήσεων Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,11 +81,11 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
 
     // Android core
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2025.07.00"))
