81c2bc17e7726089758d055b5d843aaae4c4e47b
ipergamali <joanneper@hotmail.com>
2025-08-23 21:54:46 +0300

    Χρήση Firebase BoM και ενημέρωση plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.9.3" apply false
+    id("com.android.application") version "8.5.2" apply false
     // Το plugin Compose απαιτείται σε Kotlin 2.x για να ενεργοποιηθεί ο
     // compiler του Jetpack Compose.
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
     kotlin("kapt") version "2.1.21" apply false
 
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 
