f9518ec0edf5e6d0590584ee856d5c6f384ca3d3
ipergamali <joanneper@hotmail.com>
2025-08-24 04:43:59 +0300

    Αφαίρεση των παρωχημένων Firebase KTX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("kotlin-kapt")
     id("com.google.gms.google-services")
 }
+
 // Διαβάζουμε τα API keys από το local.properties ή από μεταβλητή περιβάλλοντος
 
 val localProps = Properties()
@@ -76,9 +77,9 @@ kotlin {
 dependencies {
     // Firebase βιβλιοθήκες (BoM για συγχρονισμένες εκδόσεις)
     implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-dynamic-links-ktx")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-dynamic-links")
 
     // Android core
     implementation(libs.androidx.core.ktx)
