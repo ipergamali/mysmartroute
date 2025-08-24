1a7f7cfb368577197979a07e2308a0e23f36a3e3
ipergamali <joanneper@hotmail.com>
2025-08-24 05:18:07 +0300

    Ενημέρωση Gradle για Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.11.0" apply false
+    id("com.android.application") version "8.5.2" apply false
     // Το plugin Compose απαιτείται σε Kotlin 2.x για να ενεργοποιηθεί ο
     // compiler του Jetpack Compose.
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
-    kotlin("kapt") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+    kotlin("kapt") version "2.0.20" apply false
 
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
