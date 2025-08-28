ed45beee855f25bfd359d1b60df57ae3ef0bdf07
ipergamali <joanneper@hotmail.com>
2025-08-28 16:21:44 +0300

    Add Firebase example and use kapt plugin DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    id("kotlin-kapt")
+    kotlin("kapt")
     id("com.google.gms.google-services")
 }
 
