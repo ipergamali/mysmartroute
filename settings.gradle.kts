259c23240d74b6bf121401b3625e6769abf2ef6c
ipergamali <joanneper@hotmail.com>
2025-08-29 05:12:11 +0300

    Ενημέρωση εκδόσεων Gradle και Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,9 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.7.2" apply false
-        id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+        id("com.android.application") version "8.12.2" apply false
+        id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+        id("com.google.gms.google-services") version "4.4.3" apply false
     }
 }
 
