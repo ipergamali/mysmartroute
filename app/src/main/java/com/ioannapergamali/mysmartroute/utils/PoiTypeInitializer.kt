ab77f2c81e1f093ac98edce99c473e89a39bcfd2
ipergamali <joanneper@hotmail.com.com>
2025-09-03 07:16:33 +0300

    Revert "Handle Firestore permission errors when populating POI types"
    This reverts commit 48bd785e47027cbecbddb5f95187a3bd8e2fdfca.

--- app/src/main/java/com/ioannapergamali/mysmartroute/utils/PoiTypeInitializer.kt
+++ app/src/main/java/com/ioannapergamali/mysmartroute/utils/PoiTypeInitializer.kt
@@ -1,13 +1,11 @@
 package com.ioannapergamali.mysmartroute.utils
 
 import android.content.Context
-import android.util.Log
 import com.google.android.libraries.places.api.model.Place
 import com.google.firebase.firestore.FirebaseFirestore
 import com.ioannapergamali.mysmartroute.data.local.MySmartRouteDatabase
 import com.ioannapergamali.mysmartroute.data.local.PoiTypeEntity
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.tasks.await
 
 /**
  * Δημιουργεί τα έγγραφα της συλλογής `poi_types` στο Firestore αν δεν υπάρχουν.
@@ -18,16 +16,11 @@ fun populatePoiTypes(context: Context) {
     runBlocking {
         MySmartRouteDatabase.getInstance(context).poiTypeDao()
             .insertAll(types.map { PoiTypeEntity(it.name, it.name) })
-        for (type in types) {
-            val data = mapOf("id" to type.name, "name" to type.name)
-            try {
-                firestore.collection("poi_types")
-                    .document(type.name)
-                    .set(data)
-                    .await()
-            } catch (e: Exception) {
-                Log.e("PoiTypeInitializer", "Αποτυχία εγγραφής τύπου ${type.name}", e)
-            }
-        }
+    }
+    for (type in types) {
+        val data = mapOf("id" to type.name, "name" to type.name)
+        firestore.collection("poi_types")
+            .document(type.name)
+            .set(data)
     }
 }
