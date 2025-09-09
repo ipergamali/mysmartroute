a0fe958c0633231e315a43705966ed132b9c72fd
ipergamali <joanneper@hotmail.com>
2025-09-10 00:04:40 +0300

    Add foundation-layout for Modifier.weight

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-text")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.foundation:foundation-layout")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.navigation:navigation-compose:2.9.1")
