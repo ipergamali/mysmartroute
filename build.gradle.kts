031b02347ce7408fb1593dfcf89716ad3e203046
ipergamali <joanneper@hotmail.com>
2025-09-15 01:33:45 +0300

    Update Kotlin, KSP, Hilt and Room versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.21" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
