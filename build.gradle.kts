40d6478c4b529db2d958bbee1ac1e77cc23b3875
ipergamali <joanneper@hotmail.com.com>
2025-09-15 00:55:23 +0300

    change  version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.12.2" apply false
-
-    // Χρήση της τελευταίας σταθερής έκδοσης 1.9.24 του Kotlin
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-
-    // KSP για συμβατότητα με Kotlin 1.9.24
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-
+    id("com.android.application") version "8.7.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
